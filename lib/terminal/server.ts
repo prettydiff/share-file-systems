4ae25092f03e8a642047e594f3d37bbc2aff26a9
prettydiff <info@prettydiff.com>
2019-11-21 13:52:31 -0600

    a more precise means of security matching requests to identified shares

--- lib/terminal/server.ts
+++ lib/terminal/server.ts
@@ -102,26 +102,29 @@ const library = {
                                     windows:boolean = (data.location[0].charAt(0) === "\\" || (/^\w:\\/).test(data.location[0]) === true),
                                     readOnly:string[] = ["fs-base64", "fs-close", "fs-copy", "fs-copy-file", "fs-copy-list", "fs-copy-request", "fs-copy-self", "fs-details", "fs-directory", "fs-hash", "fs-read"];
                                 let dIndex:number = data.location.length,
-                                    sIndex:number = shares.length;
+                                    sIndex:number = shares.length,
+                                    bestMatch:number = -1;
                                 do {
                                     dIndex = dIndex - 1;
                                     sIndex = shares.length;
                                     do {
                                         sIndex = sIndex - 1;
-                                        if (sIndex < 0) {
-                                            data.location.splice(dIndex, 1);
-                                            break;
-                                        }
                                         if (data.location[dIndex].indexOf(shares[sIndex].name) === 0 || (windows === true && data.location[dIndex].toLowerCase().indexOf(shares[sIndex].name.toLowerCase()) === 0)) {
-                                            if (shares[sIndex].readOnly === true && readOnly.indexOf(data.action) < 0) {
-                                                response.writeHead(403, {"Content-Type": "text/plain; charset=utf-8"});
-                                                response.write(`{"id":"${data.id}","dirs":"readOnly"}`);
-                                                response.end();
-                                                return;
+                                            if (bestMatch < 0 || shares[sIndex].name.length > shares[bestMatch].name.length) {
+                                                bestMatch = sIndex;
                                             }
-                                            break;
                                         }
-                                    } while (sIndex > -1);
+                                    } while (sIndex > 0);
+                                    if (bestMatch < 0) {
+                                        data.location.splice(dIndex, 1);
+                                    } else {
+                                        if (shares[bestMatch].readOnly === true && readOnly.indexOf(data.action) < 0) {
+                                            response.writeHead(403, {"Content-Type": "text/plain; charset=utf-8"});
+                                            response.write(`{"id":"${data.id}","dirs":"readOnly"}`);
+                                            response.end();
+                                            return;
+                                        }
+                                    }
                                 } while (dIndex > 0);
                             }
                             if (data.location.length > 0) {
