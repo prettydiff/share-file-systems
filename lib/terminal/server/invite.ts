475ec2e027056732e58a979302c11517dda86617
prettydiff <info@prettydiff.com>
2019-11-25 19:01:09 -0600

    testing changes to invitation messaging

--- lib/terminal/server/invite.ts
+++ lib/terminal/server/invite.ts
@@ -49,42 +49,45 @@ const invite = function terminal_server_invite(dataString:string, response:http.
                         vars.ws.broadcast(errorMessage.toString());
                     });
                 });
-                request.on("error", function terminal_server_invite_inviteRequest_error(errorMessage:nodeError):void {
-                    if (errorMessage.code === "ETIMEDOUT") {
-                        if (data.action === "invite-request") {
-                            data.message = `Remote user, ip - ${serverVars.addresses[0][1][1]} and port - ${serverVars.webPort}, timed out. Invitation not sent.`;
-                            vars.ws.broadcast(`invite-error:${JSON.stringify(data)}`);
-                        } else if (data.action === "invite-complete") {
-                            data.message = `Originator, ip - ${serverVars.addresses[0][1][1]} and port - ${serverVars.webPort}, timed out. Invitation incomplete.`;
-                            vars.ws.broadcast(`invite-error:${JSON.stringify(data)}`);
-                        }
+            request.on("error", function terminal_server_invite_inviteRequest_error(errorMessage:nodeError):void {
+                if (errorMessage.code === "ETIMEDOUT") {
+                    if (data.action === "invite-request") {
+                        data.message = `Remote user, ip - ${serverVars.addresses[0][1][1]} and port - ${serverVars.webPort}, timed out. Invitation not sent.`;
+                        vars.ws.broadcast(`invite-error:${JSON.stringify(data)}`);
+                    } else if (data.action === "invite-complete") {
+                        data.message = `Originator, ip - ${serverVars.addresses[0][1][1]} and port - ${serverVars.webPort}, timed out. Invitation incomplete.`;
+                        vars.ws.broadcast(`invite-error:${JSON.stringify(data)}`);
                     }
-                    log([data.action, errorMessage.toString()]);
-                    vars.ws.broadcast(errorMessage.toString());
-                });
-                if (data.action === "invite-request") {
-                    data.ip = serverVars.addresses[0][1][1];
-                    data.port = serverVars.webPort;
                 }
-                request.write(payload);
-                request.end();
-            };
+                log([data.action, errorMessage.toString()]);
+                vars.ws.broadcast(errorMessage.toString());
+            });
+            if (data.action === "invite-request") {
+                data.ip = serverVars.addresses[0][1][1];
+                data.port = serverVars.webPort;
+            }
+            request.write(payload);
+            request.end();
+        };
+    let responseString:string;
     response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
     if (data.action === "invite") {
         data.action = "invite-request";
-        response.write("Invitation received at local terminal.");
+        responseString = `Invitation received at start terminal ${serverVars.addresses[0][1][1]} from start browser. Sending invitation to remote terminal: ${data.ip}.`;
         inviteRequest();
     } else if (data.action === "invite-request") {
         vars.ws.broadcast(`invite-request:${dataString}`);
-        response.write("Invitation sent to remote browser.");
+        responseString = `Invitation received at remote terminal ${data.ip} and sent to remote browser.`;
     } else if (data.action === "invite-response") {
         data.action = "invite-complete";
-        response.write("Invitation response processed at terminal.");
+        responseString = `Invitation response processed at remote terminal ${data.ip} and sent to start terminal.`;
         inviteRequest();
     } else if (data.action === "invite-complete") {
         vars.ws.broadcast(`invite-request:${dataString}`);
-        response.write("Invitation sent to originating browser.");
+        responseString = `Invitation sent to from start terminal ${serverVars.addresses[0][1][1]} to start browser.`;
     }
+    log([responseString]);
+    response.write(responseString);
     response.end();
 };
 
