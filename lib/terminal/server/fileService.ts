7accf8c81574f7ed44747bf36a9a7861bea01c8a
prettydiff <info@prettydiff.com>
2019-12-05 10:03:11 -0600

    fixing a bad escape in JSON parsing, due to the Windows directory separator the back slash

--- lib/terminal/server/fileService.ts
+++ lib/terminal/server/fileService.ts
@@ -302,7 +302,7 @@ const library = {
                         library.log([``]);
                         cut();
                         response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
-                        vars.ws.broadcast(`fileListStatus:{"failures":${JSON.stringify(hashFail)},"target":"local-${data.name}","message":"Copy complete. ${library.commas(countFile)} file${filePlural} written at size ${library.prettyBytes(writtenSize)} (${library.commas(writtenSize)} bytes) with ${hashFailLength} failure${hashFailPlural}."}`);
+                        vars.ws.broadcast(`fileListStatus:{"failures":${JSON.stringify(hashFail)},"target":"local-${data.name.replace(/\\/g, "\\\\")}","message":"Copy complete. ${library.commas(countFile)} file${filePlural} written at size ${library.prettyBytes(writtenSize)} (${library.commas(writtenSize)} bytes) with ${hashFailLength} failure${hashFailPlural}."}`);
                         response.write(`fileListStatus:{"failures":${JSON.stringify(hashFail)},"target":"remote-${fileData.id}","message":"Copy complete. ${library.commas(countFile)} file${filePlural} written at size ${library.prettyBytes(writtenSize)} (${library.commas(writtenSize)} bytes) with ${hashFailLength} failure${hashFailPlural}."}`);
                         response.end();
                     },
@@ -325,7 +325,7 @@ const library = {
                                 countFile = countFile + 1;
                                 writtenFiles = writtenFiles + 1;
                                 writtenSize = writtenSize + fileQueue[index][1];
-                                vars.ws.broadcast(`fileListStatus:{"failures":[],"target":"local-${data.name}","message":"Copying ${((writtenSize / fileData.fileSize) * 100).toFixed(2)}% complete. ${countFile} file${filePlural} written at size ${library.prettyBytes(writtenSize)} (${library.commas(writtenSize)} bytes) and ${library.commas(hashFailLength)} integrity failure${hashFailPlural}."}`);
+                                vars.ws.broadcast(`fileListStatus:{"failures":[],"target":"local-${data.name.replace(/\\/g, "\\\\")}","message":"Copying ${((writtenSize / fileData.fileSize) * 100).toFixed(2)}% complete. ${countFile} file${filePlural} written at size ${library.prettyBytes(writtenSize)} (${library.commas(writtenSize)} bytes) and ${library.commas(hashFailLength)} integrity failure${hashFailPlural}."}`);
                             }
                             if (index < fileQueue.length - 1) {
                                 terminal_server_fileService_requestFiles_writeFile(index + 1);
@@ -362,7 +362,7 @@ const library = {
                                     ? ""
                                     : "s",
                                 written:number = writeStream.bytesWritten + writtenSize;
-                            vars.ws.broadcast(`fileListStatus:{"failures":[],"target":"local-${data.name}","message":"Copying ${((written / fileData.fileSize) * 100).toFixed(2)}% complete for ${fileData.fileCount} files. ${countFile} file${filePlural} written at size ${library.prettyBytes(written)} (${library.commas(written)} bytes) and ${library.commas(hashFailLength)} integrity failure${hashFailPlural}."}`);
+                            vars.ws.broadcast(`fileListStatus:{"failures":[],"target":"local-${data.name.replace(/\\/g, "\\\\")}","message":"Copying ${((written / fileData.fileSize) * 100).toFixed(2)}% complete for ${fileData.fileCount} files. ${countFile} file${filePlural} written at size ${library.prettyBytes(written)} (${library.commas(written)} bytes) and ${library.commas(hashFailLength)} integrity failure${hashFailPlural}."}`);
                         });
                         fileResponse.on("end", function terminal_server_fileService_requestFiles_writeStream_end():void {
                             const hashStream:fs.ReadStream = vars.node.fs.ReadStream(filePath);
@@ -434,7 +434,7 @@ const library = {
                                 hashFailPlural:string = (hashFailLength === 1)
                                     ? ""
                                     : "s";
-                            data.id = `fileListStatus:{"failures":[],"target":"local-${fileData.id}","message":"Copying ${((writtenSize / fileData.fileSize) * 100).toFixed(2)}% complete. ${countFile} file${filePlural} written at size ${library.prettyBytes(writtenSize)} (${library.commas(writtenSize)} bytes) and ${library.commas(hashFailLength)} integrity failure${hashFailPlural}."}`;
+                            data.id = `fileListStatus:{"failures":[],"target":"local-${data.name.replace(/\\/g, "\\\\")}","message":"Copying ${((writtenSize / fileData.fileSize) * 100).toFixed(2)}% complete. ${countFile} file${filePlural} written at size ${library.prettyBytes(writtenSize)} (${library.commas(writtenSize)} bytes) and ${library.commas(hashFailLength)} integrity failure${hashFailPlural}."}`;
                         }
                         data.location = [fileData.list[a][0]];
                         data.remoteWatch = fileData.list[a][2];
@@ -479,7 +479,7 @@ const library = {
                     const filePlural:string = (fileData.fileCount === 1)
                         ? ""
                         : "s";
-                    vars.ws.broadcast(`fileListStatus:{"failures":[],"target":"local-${data.name}","message":"Copy started for ${fileData.fileCount} file${filePlural} at ${library.prettyBytes(fileData.fileSize)} (${library.commas(fileData.fileSize)} bytes)."}`);
+                    vars.ws.broadcast(`fileListStatus:{"failures":[],"target":"local-${data.name.replace(/\\/g, "\\\\")}","message":"Copy started for ${fileData.fileCount} file${filePlural} at ${library.prettyBytes(fileData.fileSize)} (${library.commas(fileData.fileSize)} bytes)."}`);
                 }
                 if (fileData.list[0][1] === "directory") {
                     makeDir();
