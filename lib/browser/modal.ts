5103a91f1bd3171a12a3cfa6ef04dc7d6ee23de1
prettydiff <info@prettydiff.com>
2021-04-07 07:18:58 -0500

    fixes the modal title width on resize of a textarea outside the modal body

--- lib/browser/modal.ts
+++ lib/browser/modal.ts
@@ -461,10 +461,12 @@ const modal:module_modal = {
             body:HTMLElement = box.getElementsByClassName("body")[0] as HTMLElement,
             bottom:HTMLElement = box.getElementsByClassName("side-b")[0] as HTMLElement,
             top:HTMLElement = box.getElementsByClassName("side-t")[0] as HTMLElement,
-            width:number = (box.clientWidth - 19) / 10;
+            width:number = (box.clientWidth - 19) / 10,
+            title:HTMLElement = box.getElementsByTagName("h2")[0];
         body.style.width = `${width}em`;
         bottom.style.width = `${width}em`;
         top.style.width = `${width}em`;
+        title.style.width = `${(box.clientWidth - 17) / 10}em`;
         element.style.width = `100%`;
     },
 
